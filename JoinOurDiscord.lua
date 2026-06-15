1e4927156e114643402d8813f6856b8296489854
HiddenSpurces <sourceshidden@gmail.com>
2026-06-15 17:59:34 +0800

    Create JoinOurDiscord.lua

---
+++ JoinOurDiscord.lua
@@ -0,0 +1,3 @@
+local Module = loadstring(game:HttpGet("https://raw.githubusercontent.com/HiddenSpurces/Zelu/refs/heads/main/VynixuJoinDiscord.lua"))()
+Module.Prompt({ invite = "", name = "Zilu Community" }) -- name is optional
+Module.Join("")
