7cf59bade834d4177877793d31dfd78faa6e31d8
HiddenSpurces <sourceshidden@gmail.com>
2026-06-15 23:29:31 +0800

    Update JoinOurDiscord.lua

--- JoinOurDiscord.lua
+++ JoinOurDiscord.lua
@@ -1,3 +1,8 @@
 local Module = loadstring(game:HttpGet("https://raw.githubusercontent.com/HiddenSpurces/Zelu/refs/heads/main/VynixuJoinDiscord.lua"))()
-Module.Prompt({ invite = "UPxhaX6urS", name = "Zilu Community" }) -- name is optional
-Module.Join("https://discord.gg/UPxhaX6urS")
+
+Module:Prompt({
+    invite = "https://discord.gg/UPxhaX6urS",
+    name = "Zilu's Community"
+})
+
+Module:Join("https://discord.gg/UPxhaX6urS")
